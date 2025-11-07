4ed37feb2709f7c9e896d71baf661baa4d3ac1ef
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-11-07 23:08:33 +0100

    minigbm-upstream: Wire up gbm_mesa
    [cafebabe: We don't use gralloc0 and gralloc4 in this repo, reorder some code, add _upstream suffix]
    
    Change-Id: Icc2236c4820a5b5fb418fa2f5c2cc8cba7275111

--- drv.c
+++ drv.c
@@ -31,6 +31,9 @@
 #ifdef DRV_AMDGPU
 extern const struct backend backend_amdgpu;
 #endif
+#ifdef DRV_GBM_MESA
+extern const struct backend backend_gbm_mesa;
+#endif
 #ifdef DRV_I915
 extern const struct backend backend_i915;
 #endif
@@ -92,16 +95,21 @@ static const struct backend *drv_backend_list[] = {
 
 void drv_preload(bool load)
 {
+#ifndef DRV_GBM_MESA
 	unsigned int i;
 	for (i = 0; i < ARRAY_SIZE(drv_backend_list); i++) {
 		const struct backend *b = drv_backend_list[i];
 		if (b->preload)
 			b->preload(load);
 	}
+#endif
 }
 
 static const struct backend *drv_get_backend(int fd)
 {
+#ifdef DRV_GBM_MESA
+	return &backend_gbm_mesa;
+#else
 	drmVersionPtr drm_version;
 	unsigned int i;
 
@@ -122,6 +130,7 @@ static const struct backend *drv_get_backend(int fd)
 
 	drv_loge("no matching backend, using dumb backend as fallback\n");
 	return &backend_dumb_generic;
+#endif
 }
 
 struct driver *drv_create(int fd, const struct backend *backend)
