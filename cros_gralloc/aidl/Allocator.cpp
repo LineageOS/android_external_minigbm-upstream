1dc0aaf9e9ce91d2bd5b300a4a3a51792a780c49
Yumi Yukimura <me.cafebabe@gmail.com>
2025-12-05 18:04:46 +0800

    minigbm-upstream: Add "_upstream" suffix to module names
    * To avoid conflicts with AOSP bundled external/minigbm
    
    Change-Id: Id519f06d4f4bb06f4441414a195d36f448bdf57d

--- cros_gralloc/aidl/Allocator.cpp
+++ cros_gralloc/aidl/Allocator.cpp
@@ -198,7 +198,7 @@ ndk::ScopedAStatus Allocator::isSupported(const BufferDescriptorInfo& descriptor
 }
 
 ndk::ScopedAStatus Allocator::getIMapperLibrarySuffix(std::string* outResult) {
-    *outResult = "minigbm";
+    *outResult = "minigbm_upstream";
     return ndk::ScopedAStatus::ok();
 }
 
